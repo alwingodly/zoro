f3542d5195ea06938e4bf691029b0dc0782307c3
alwingodly <alwinaswin123@gmail.com>
2025-10-19 19:04:12 +0530

    error changr

--- backend/src/server.js
+++ backend/src/server.js
@@ -30,7 +30,7 @@ app.get("/", (req, res) => {
 // Error handler
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).json({ message: "Internal server error" });
+  res.status(500).json({ message: "Internal server error of alwin" });
 });
 
 // Local run only
