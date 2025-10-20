d1757c8c791b9253219e5695412e4fabb05333d9
alwingodly <alwinaswin123@gmail.com>
2025-10-20 15:01:26 +0530

    create post

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -1,15 +1,23 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React, { use } from 'react'
+import { ScrollView, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import SignOutButton from '@/components/SignOutButton'
 import { useUserSync } from '@/hooks/useUserSync'
+import { MaterialCommunityIcons } from '@expo/vector-icons'
+import PostComposer from '@/components/PostComposer'
 
 const HomeScreen = () => {
   useUserSync();
   return (
-    <SafeAreaView style={{flex:1}}>
-      <Text>HomeScreen</Text>
-      <SignOutButton />
+    <SafeAreaView style={{ flex: 1 }}>
+      <View className='flex-row justify-between items-center px-4 py-3 vorder-b boder-gray-100'>
+        <MaterialCommunityIcons name="alpha-z-circle" size={32} color={"#3BAF56"} />
+        <Text className='text-xl font-bold text-gray-900'>Home</Text>
+        <SignOutButton />
+      </View>
+      <ScrollView showsVerticalScrollIndicator={false} className='flex-1' contentContainerStyle={{ paddingBottom: 80 }}>
+       <PostComposer/>
+      </ScrollView>
     </SafeAreaView>
   )
 }
